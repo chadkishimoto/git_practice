1766987feca7f9ca7caddb216d1831a6ceb8c6c6
Chad Kishimoto <ckishimoto@sandiego.edu>
2022-07-06 15:29:48 -0700

    Initial commit

---
+++ Francisco.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+def plot_me(x):
+    return 1.0 * x**0
+
+plot_color = "green"
---
+++ Kathryn.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+def plot_me(x):
+    return 0.5 * x ** 0
+
+plot_color = "blue"
